6e749489e5c5e83614396ea18ff97d5db3b820d3
cipdv <cip.devries@gmail.com>
2020-03-24 14:58:16 -0400

    finished inital google oauth setup

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require ('express');
 const app = express ();
+const passport = require ('passport');
+require ('./services/passport');
+require ('./routes/authRoutes')(app);
+
+
+
+
 
 app.get('/', (req, res)=> {
     res.send('hello');
 })
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen (PORT);
